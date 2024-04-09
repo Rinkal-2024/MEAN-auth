ea528b79f1f411bb5cd141f82bb9d2cd3f93cc06
Rinkal-2024 <rinkal.dhankariya@iflair.com>
2024-04-09 12:10:25 +0530

    update

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const routes = require('./routes/routes')
 const app = express();
 app.use(cors({
     credentials:true,
-    origin:['https://mean-auth1.vercel.app']
+    origin:['https://mean-auth-jwtauth.vercel.app']
 }))
 app.use(cookieParser())
 app.use(express.json())
